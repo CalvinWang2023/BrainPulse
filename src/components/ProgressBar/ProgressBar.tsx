bfe6c24b4e36b0f652ce56ff87615818309b7686
Yanchun Wang <wang146@mcmaster.ca>
2023-09-15 09:16:23 -0400

    Create a progressbar

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import './ProgressBar.css';
+
+interface ProgressBarProps {
+    total: number;
+    current: number;
+    isTimer?: boolean; // Flag to indicate progressbar used as timer (default to false)
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ total, current, isTimer = false }) => {
+    const percentage = (current / total) * 100;
+
+    return (
+        <div className="progress-bar">
+            <div className={`progress ${isTimer ? 'timer-bar' : '' }`} style={{ width: `${percentage}%` }}>
+                {percentage > 0 && !isTimer && `${percentage}%`}
+            </div>
+        </div>
+    )
+}
+
+export default ProgressBar
