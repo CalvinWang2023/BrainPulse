8d505157d8206c492f5a1d06c05ba4b7e075e769
Yanchun Wang <wang146@mcmaster.ca>
2023-09-16 19:17:18 -0400

    Add a progressbar to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,26 @@
 import "./Navbar.css";
 import { JoyStick } from "../../icons/LogoIcons";
 import ProgressBar from "../ProgressBar/ProgressBar";
+import { useAppSelector } from "../../app/hooks";
 
 interface NavbarProps {
     quizInProgress: boolean; // Flag to determine if quiz is in progress
 }
 
 const Navbar: React.FC<NavbarProps> = ({ quizInProgress }) => {
-
+    const { questions, currentIndex } = useAppSelector((state) => state.quiz);
     return (
         <nav className="nav">
             <div className="nav-logo">
                 <JoyStick />
                 <h1>BrainPulse</h1>
             </div>
+
+            {quizInProgress && <ProgressBar total={questions.length} current={currentIndex} />}
+
             <div className="nav-button">
                 <button>demo</button>
             </div>
-
-            {/* {quizInProgress && <ProgressBar total={} current={} isTimer={true} />} */}
-
         </nav>
     );
 }
