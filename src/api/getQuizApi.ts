8b8d56de3deba1af1ccde06d8606d72187e6aa40
Yanchun Wang <wang146@mcmaster.ca>
2023-09-16 18:02:27 -0400

    Update getQuizApi

--- src/api/getQuizApi.ts
+++ src/api/getQuizApi.ts
@@ -1,9 +1,9 @@
 import get from 'axios';
 
 const API_URL = 'https://opentdb.com/api.php';
-const getQuestionsProxy = async (params: FormState) => {
+const getQuizApi = async (params: FormState) => {
   const { results } = (await get(API_URL, { params, timeout: 5000 })).data;
   return results;
 };
 
-export default getQuestionsProxy;
+export default getQuizApi;
