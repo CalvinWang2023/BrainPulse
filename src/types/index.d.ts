ba694726a1bcf23da8fe72bacaa957338a57f88c
Yanchun Wang <wang146@mcmaster.ca>
2023-09-16 17:52:40 -0400

    Add appropriate types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,31 @@
 type Timer = {
     initialTime: number;
     elapsedTime: number;
+    delay: number;
     isTimerStop: boolean;
+};
+
+type FormState = {
+    amount: '5' | '10' | '15' | '20';
+    category: string;
+    difficulty: 'easy' | 'medium' | 'hard';
+    type: 'multiple' | 'boolean';
+};
+
+type FormValue = FormState[keyof FormState];
+
+type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: 'easy' | 'medium' | 'hard';
+    incorrect_answers: string[];
+    question: string;
+    type: 'multiple' | 'boolean';
+};
+
+type QuizState = {
+    questions: Question[];
+    isLoading: boolean;
+    currentIndex: number;
+    picked: string;
 };
