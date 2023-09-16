9d91ccdd79f4d2c4ad1cb3afd47885e1814bd2c4
Yanchun Wang <wang146@mcmaster.ca>
2023-09-16 17:55:56 -0400

    Add a categoryInput to setupForm

---
+++ src/components/SetupForm/components/CategoryInput.tsx
@@ -0,0 +1,53 @@
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import { setForm } from "../../../slices/formSlice";
+
+const CategoryInput = () => {
+    const dispatch = useAppDispatch();
+    const category = useAppSelector((state) => state.form.category);
+
+    const handleClick = (e: React.ChangeEvent) => {
+        const selectedCtg = e.target as HTMLOptionElement;
+        dispatch(setForm({
+            name: "category",
+            value: selectedCtg.value
+        }));
+    };
+    return (
+        <div className="setup-container">
+            <label>{`Select Category:`}</label>
+            <select
+                name="category"
+                value={category} 
+                className="ctgs-group" 
+                onChange={handleClick}
+            >
+                <option value="9">General Knowledge</option>
+                <option value="10">Books</option>
+                <option value="11">Movies</option>
+                <option value="14">TV Shows</option>
+                <option value="26">Celebrities</option>
+                <option value="12">Music</option>
+                <option value="13">Musicals &amp; Theatres</option>
+                <option value="15">Video Games</option>
+                <option value="16">Board Games</option>
+                <option value="17">Science &amp; Nature</option>
+                <option value="27">Animals</option>
+                <option value="28">Vehicles</option>
+                <option value="18">Computers</option>
+                <option value="30">Gadgets</option>
+                <option value="19">Mathematics</option>
+                <option value="20">Mythology</option>
+                <option value="21">Sports</option>
+                <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="29">Comics</option>
+                <option value="31">Japanese Anime &amp; Manga</option>
+                <option value="32"> Cartoon &amp; Animations</option>
+                <option value="25">Art</option>
+            </select>
+        </div>
+    )
+}
+
+export default CategoryInput;
