6d3a2261d39997697ef09305ac487968421ed4fa
Yanchun Wang <wang146@mcmaster.ca>
2023-09-16 17:53:33 -0400

    Add a formslice

---
+++ src/slices/formSlice.ts
@@ -0,0 +1,27 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const initialState: FormState = {
+    amount: "5",
+    category: "9",
+    difficulty: "easy",
+    type: "multiple",
+};
+
+const formSlice = createSlice({
+    name: "timer",
+    initialState,
+    reducers: {
+        setForm(state, { payload: { name, value } }: PayloadAction<{
+            name: keyof FormState,
+            value: FormValue,
+        }>) {
+            return { ...state, [name]: value };
+        },
+    },
+});
+
+export const {
+    setForm,
+} = formSlice.actions;
+
+export default formSlice.reducer;
