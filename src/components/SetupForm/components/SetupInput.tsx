4fd5f06b34f3ef6867d94b7d3f5efc30118d7b37
Yanchun Wang <wang146@mcmaster.ca>
2023-09-16 17:55:27 -0400

    Encapsulate a component of setupForm This is to avoid repeating

---
+++ src/components/SetupForm/components/SetupInput.tsx
@@ -0,0 +1,48 @@
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import { setForm } from "../../../slices/formSlice";
+
+interface InputProps {
+    label: string;
+    choices: Array<FormValue>;
+    FormKey: keyof FormState;
+    altChoiceName?: Array<string>; 
+}
+
+const SetupInput: React.FC<InputProps> = ({ label, choices, FormKey, altChoiceName = null }) => {
+    const dispatch = useAppDispatch();
+    const form = useAppSelector((state) => state.form);
+
+    const handleClick = (e: React.MouseEvent) => {
+        const selectedChoice = e.target as HTMLButtonElement;
+        dispatch(setForm({
+            name: FormKey,
+            value: selectedChoice.value
+        }));
+    };
+
+    return (
+        <div className="setup-container">
+            <label>{`Select ${label}:`}</label>
+            <ul>
+                {choices.map((choice, index) => {
+                    return (
+                        <li key={index}>
+                            <button 
+                                className={form[FormKey] === choice ? `active` : `inactive`}
+                                value={choice} 
+                                onClick={handleClick}
+                            >
+                                {altChoiceName === null ? 
+                                    choice 
+                                    : altChoiceName[index]
+                                }
+                            </button>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default SetupInput;
