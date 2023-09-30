5200ecf97d42eafd2877ff211e570f49c8c80836
Yanchun Wang <wang146@mcmaster.ca>
2023-09-29 21:06:29 -0400

    Change Navbar in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import SetupForm from './components/SetupForm/SetupForm';
 import Quiz from './components/Quiz/Quiz';
+import Results from './components/Results/Results';
 import Layout from './Layout';
 
 function App() {
     return (
         <div className="App">
-            <Navbar quizInProgress={true} />
+            <Navbar />
             <Routes>
                 <Route path='/' element={ <Layout /> }>
                     <Route path='/' element={ <SetupForm /> } />
                     <Route path='/Quiz' element={ <Quiz /> } />
+                    <Route path='/Results' element={ <Results /> } />
                 </Route>
             </Routes>
         </div>
